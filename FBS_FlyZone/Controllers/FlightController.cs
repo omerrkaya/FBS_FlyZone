005d36f42d63d12cc8d8c485dd0b3918e9682823
omerrkaya <omerkaya1761@gmail.com>
2025-03-01 16:50:16 +0300

    Front parçalama, yüklenme animasyonu

---
+++ FBS_FlyZone/Controllers/FlightController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FBS_FlyZone.Controllers
+{
+    public class FlightController : Controller
+    {
+        public IActionResult Flight()
+        {
+            return View();
+        }
+    }
+}
--- FBS_FlyZone/Program.cs
+++ FBS_FlyZone/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Flight}/{action=Flight}/{id?}");
 
 app.Run();
