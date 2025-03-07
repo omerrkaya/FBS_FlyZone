d8137b8d352726a9551af780919ff416e05fc52d
İpek Gökçe <ipekgokce2005@gmail.com>
2025-03-07 15:45:38 +0300

    ...

--- EntityLayer/Concrete/Aircrafts.cs
+++ EntityLayer/Concrete/Aircrafts.cs
@@ -14,7 +14,7 @@ namespace EntityLayer.Concrete
         public int AircraftID { get; set; }
         [ForeignKey("Aircraft_Firm")]
         public int Aircraft_Firm { get; set; }
-        [Required, StringLength(54)]
+        [Required, StringLength(50)]
         public string Aircraft_Model { get; set; }
         public int Number_Of_Seats { get; set; }
         public int Range_KM { get; set; }
