724c87da1e03bcdaa2ee50e31fc6c2b19d22368f
omerrkaya <omerkaya1761@gmail.com>
2025-04-03 20:39:22 +0300

    ....

---
+++ FBS_FlyZone/Controllers/Deneme.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FBS_FlyZone.Controllers
+{
+    public class Deneme : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
