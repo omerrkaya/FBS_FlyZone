d8fe2946d4f26de32e333e31823c9a960d2ded64
omerrkaya <omerkaya1761@gmail.com>
2025-04-09 18:34:03 +0300

    .

---
+++ FBS_FlyZone/Controllers/TravelExperienceController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FBS_FlyZone.Controllers
+{
+    public class TravelExperienceController : Controller
+    {
+        public IActionResult BusinessClass()
+        {
+            return View();
+        }
+        public IActionResult EconomyClass()
+        {
+            return View();
+        }
+
+        public IActionResult InFlightCatering()
+        {
+
+            return View();
+        }
+
+        public IActionResult InFlightEntertainment()
+        {
+            return View();
+        }
+
+    }
+}
