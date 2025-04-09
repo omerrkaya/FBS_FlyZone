b55c681dbe3abb0329de899260ea630aaf16870f
omerrkaya <omerkaya1761@gmail.com>
2025-04-09 17:09:37 +0300

    Login-Register tamam kimse ikisine de dokunmasın

--- FBS_FlyZone/Program.cs
+++ FBS_FlyZone/Program.cs
@@ -1,4 +1,33 @@
+
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddSession();
+
+
+//builder.Services.AddMvc(config =>
+//{
+//    var policy = new AuthorizationPolicyBuilder()
+//        .RequireAuthenticatedUser()
+//        .Build();
+//    config.Filters.Add(new AuthorizeFilter(policy));
+//});
+
+//builder.Services.AddMvc();
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//  .AddCookie(option =>
+//  {
+//      option.LoginPath = "/Account/Login";
+//  });
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -15,6 +44,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+//app.UseAuthentication();
+
+//app.UseSession();
 
 app.UseRouting();
 
