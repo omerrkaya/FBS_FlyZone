5e97e6999e08d2d59e667c46a29380a1a858b784
omerrkaya <omerkaya1761@gmail.com>
2025-04-13 15:08:19 +0300

    filtreleme tamam

--- FBS_FlyZone/Models/FlightSearchViewModel .cs
+++ FBS_FlyZone/Models/FlightSearchViewModel .cs
@@ -1,22 +1,27 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 public class FlightSearchViewModel
 {
-    [Required(ErrorMessage = "Kalkış yeri zorunludur")]
-    public string? FromLocation { get; set; }
 
-    [Required(ErrorMessage = "Varış yeri zorunludur")] 
-    public string? ToLocation { get; set; }
+    [Required(ErrorMessage = "Kalkış yeri zorunludur")]
+    public int DepartureAirportId { get; set; }
+
+    [Required(ErrorMessage = "Varış yeri zorunludur")]
+    public int ArrivalAirportId { get; set; }
 
     [Required(ErrorMessage = "Gidiş tarihi zorunludur")]
     public DateTime DepartureDate { get; set; } = DateTime.Now;
 
-    public DateTime? ReturnDate { get; set; }
+    public DateTime? ReturnDate { get; set; } // opsiyonel, sadece gidiş-dönüş seçildiyse gelir
+
+    [Required(ErrorMessage = "Sınıf seçimi zorunludur"), ]
+    public string? FlightClass { get; set; }
 
     [Range(1, 9, ErrorMessage = "Yetişkin sayısı en fazla 9 olabilir")]
-    public int AdultCount { get; set; } = 1;
+    public int AdultCount { get; set; }
 
     [Range(0, 9, ErrorMessage = "Çocuk sayısı en fazla 9 olabilir")]
-    public int ChildCount { get; set; } = 0;
+    public int ChildCount { get; set; }
 }
