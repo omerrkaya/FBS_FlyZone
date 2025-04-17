10366502146283acbe7491ca0d7a87a880687955
Umut <arazumut349@gmail.com>
2025-04-17 18:00:04 +0300

    ödeme sistemi tasarımı yazıldı. controller özellikleri yazıldı.

---
+++ FBS_FlyZone/Controllers/PaymentController.cs
@@ -0,0 +1,40 @@
+//PaymentController.cs ödeme sayfası controller ı.
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using FBS_FlyZone.Models;
+using System;
+
+namespace FBS_FlyZone.Controllers
+{
+    [AllowAnonymous]
+    public class PaymentController : Controller 
+    {
+        [HttpGet] //get işlemi yapılır.
+        public IActionResult Index()
+        {
+            return View(new PaymentViewModel()); //ödeme sayfasına yönlendirilir.
+        }
+        
+        [HttpPost] //post işlemi yapılır.
+        public IActionResult Index(PaymentViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model); //model hatalı ise view içine geri döner. Modele yanlış veri döndüğünde çalışır.
+                //hata mesajlarının görüntülenmesi için.
+            }
+            
+            // Gerçek uygulamada ödeme işlemleri burada yapılır
+            
+            // Başarılı ödeme simülasyonu, gerçek veri olmasa da ödeme işlemini simüle eder. k
+            TempData["SuccessMessage"] = "Ödemeniz başarıyla alındı. Biletiniz mail adresinize gönderildi.";
+            
+            return RedirectToAction("PaymentSuccess"); //başarılı ödeme sayfasına yönlendirilir. Gerekirse burası değiştirilebilir(başarılı ödeme sayfası).
+        }
+        
+        public IActionResult PaymentSuccess() //başarılı ödeme sayfasına yönlendirilir. Gerekirse burası değiştirilebilir.
+        {
+            return View();
+        }
+    }
+} 
