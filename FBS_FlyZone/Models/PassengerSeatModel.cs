b22d4989fe7d229999c18e46db716a09f04e9c3f
omerrkaya <omerkaya1761@gmail.com>
2025-04-28 18:02:17 +0300

    Koltuk seçimi artık yapılabiliyor

---
+++ FBS_FlyZone/Models/PassengerSeatModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FBS_FlyZone.Models
+{
+    public class PassengerSeatModel
+    {
+        public int passengerId { get; set; }
+        public int reservationId { get; set; }
+        public string seatNumber { get; set; }
+    }
+
+}
