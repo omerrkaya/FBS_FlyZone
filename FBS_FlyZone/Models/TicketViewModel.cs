27afb8c4aa4254aaab82208b07f9ed2f1d7b75d7
İpek Gökçe <ipekgokce2005@gmail.com>
2025-05-04 23:31:53 +0300

    Biletlerim kısmı yapıldı.

---
+++ FBS_FlyZone/Models/TicketViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FBS_FlyZone.Models.ViewModels
+{
+    public class TicketViewModel
+    {
+        public string PassengerName { get; set; }
+        public string FlightNumber { get; set; }
+        public string Departure { get; set; }
+        public string Arrival { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public string SeatNumber { get; set; }
+        public string TicketNumber { get; set; }
+        public string Airline { get; set; }
+    }
+}
+
